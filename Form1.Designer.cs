2a4870ce65f332f25a879a42862e2021399e6b5e
helenajr <63923671+helenajr@users.noreply.github.com>
2021-05-08 12:16:54 +0100

    Successful button2. Creates a text file at a pre-specified location and adds content to the file. Says what it has done in the listBox.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace WinFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(179, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(557, 349);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,15 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        string winDir = System.Environment.GetEnvironmentVariable("windir");
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //How to read a text file.
+            //try...catch is to deal with a 0 byte file.
+            this.listBox1.Items.Clear();
+            StreamReader reader = new StreamReader(winDir + "\\system.ini");
+            try
+            {
+                do
+                {
+                    addListItem(reader.ReadLine());
+                }
+                while (reader.Peek() != -1);
+            }
+            catch
+            {
+                addListItem("File is empty");
+            }
+            finally
+            {
+                reader.Close();
+            }
+            
+        }
+        private void addListItem(string value)
+        {
+            this.listBox1.Items.Add(value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Demonstrates how to create and write to a text file.
+            StreamWriter writer = new StreamWriter("C:/Users/HRo/Desktop/TestFiles/KBTest.txt"); //This creates the file at this filepath
+            writer.WriteLine("File created using StreamWriter class."); //This adds content to the file
+            writer.Close(); //This stops adding content to the file
+            this.listBox1.Items.Clear(); //This clears the listbox
+            addListItem("File Written to C:/Users/HRo/Desktop/TestFiles/KBTest.txt"); //This is the message displayed in the listbox
+        }
     }
 }
